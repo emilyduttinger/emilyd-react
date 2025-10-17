09c2ca8105253be0631745840ff65a09ede0401f
Emily Duttinger <emily.duttinger@zaneray.com>
2025-10-17 15:39:24 -0400

    Move Header component to Layout folder, update to use utility classes and remove CSS file

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,9 +1,8 @@
-import './Header.css';
 import Navigation from "@/components/Navigation/Navigation";
 
 export default function Header() {
   return (
-    <header className="header flex flex-col justify-center items-center text-center sm:flex-row sm:justify-between sm:text-left">
+    <header className="flex flex-col justify-center items-center text-center gap-[1.5rem] px-[var(--container-padding)] py-[1.5rem] sm:flex-row sm:justify-between sm:text-left sm:px-[2.5rem]">
       <span className="heading-3 m-0">Emily Duttinger</span>
       <Navigation />
     </header>
