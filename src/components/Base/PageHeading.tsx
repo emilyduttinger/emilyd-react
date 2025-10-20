2e7865dcf6cd3eac90d2b9a3688269a1f3d7c076
Emily Duttinger <emily.duttinger@zaneray.com>
2025-10-19 22:22:48 -0400

    Add fade transition to cards

--- src/components/Base/PageHeading.tsx
+++ src/components/Base/PageHeading.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import Container from "../Container/Container";
+import { motion } from "motion/react"
 
 export interface PageHeadingProps {
   heading: string;
@@ -6,10 +9,15 @@ export interface PageHeadingProps {
 
 export default function PageHeading({heading}: PageHeadingProps) {
   return (
-    <div className="pt-[var(--stacked-component-lg)]">
+    <motion.div 
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y:0 }}
+      transition={{ delay: 0, duration: 0.5, ease: "easeInOut" }}
+      className="pt-[var(--stacked-component-lg)]"
+    >
       <Container>
         <h1 className="mb-0 heading-lg">{heading}</h1>
       </Container>
-    </div>
+    </motion.div>
   )
 }
--- src/components/Work/CardMedia.tsx
+++ src/components/Work/CardMedia.tsx
@@ -1,19 +1,24 @@
 import Image from "next/image";
 import VideoPlayer from "../Base/VideoPlayer";
+import { LazyMotion, domAnimation } from "motion/react"
+import * as m from "motion/react-m"
 
 export interface CardMediaProps {
   videoUrl?: string;
   imageSrc: string;
   imageAlt?: string;
   className?: string;
+  isVisible?: boolean;
 }
 
-export default function CardMedia({ videoUrl, imageSrc, imageAlt = "", className = "" }: CardMediaProps) {
+export default function CardMedia({ videoUrl, imageSrc, imageAlt = "", className = "", isVisible }: CardMediaProps) {
   return (
-    <div className={`rounded-sm overflow-hidden ${className}`}>
-      {
-        videoUrl ? <VideoPlayer videoUrl={videoUrl} poster={imageSrc} /> : <Image src={imageSrc} alt={imageAlt} width={1600} height={900} />
-      }
-    </div>
+    <LazyMotion features={domAnimation}>
+      <m.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ amount: 0.5 }} className={`rounded-sm overflow-hidden ${className}`}>
+        {
+          videoUrl ? <VideoPlayer videoUrl={videoUrl} poster={imageSrc} /> : <Image src={imageSrc} alt={imageAlt} width={1600} height={900} />
+        }
+      </m.div>
+    </LazyMotion>
   );
 }
