f5d80029dea69470a027f6ef42be48f692b13108
Emily Duttinger <emily.duttinger@zaneray.com>
2025-10-19 22:13:26 -0400

    Add framer motion package, add motion animations to homepage intro

--- src/components/HomeIntro/HomeIntro.tsx
+++ src/components/HomeIntro/HomeIntro.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import Container from "@/components/Container/Container";
 import styles from './HomeIntro.module.css';
 import Button from "../Base/Button";
 import VideoPlayer from "../Base/VideoPlayer";
+import { motion } from "motion/react"
 
 export default function HomeIntro() {
   const toolKitArray = ['JavaScript', 'TypeScript', 'Vue.js', 'Nuxt.js', 'React', 'HTML5 + CSS3', 'Sass + Less', 'Liquid', 'PHP', 'GraphQL', 'REST API', 'Git', 'Shopify Plus', 'Storybook', 'Design Systems', 'Accessibility', 'WCAG', 'Animation', 'UX/UI Design', 'Figma', 'Adobe Creative Suite', 'Cursor', 'Copilot'];
@@ -10,12 +13,22 @@ export default function HomeIntro() {
     <div className={styles.homeIntro}>
     <Container>
       <div className={styles.intro}>
-        <div className={styles.introText}>
+        <motion.div 
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y:0 }}
+          transition={{ delay: 0, duration: 0.5, ease: "easeInOut" }}
+          className={styles.introText}
+        >
           <h1>I build user-focused applications with a passion for clean code and elegant design</h1>
           <p className="large">I'm a <strong>Frontend UI/UX Engineer</strong> specializing in creating engaging and accessible web experiences. I'm a firm believer in the power of design systems to create scalable and consistent user interfaces.</p>
           <Button linkPath="#work" arrowDirection="down" className="mt-[var(--heading-margin)]">View My Work</Button>
-        </div>
-        <div className={styles.toolkit}>
+        </motion.div>
+        <motion.div 
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.3, duration: 0.5, ease: "easeInOut" }}
+          className={styles.toolkit}
+        >
           <h2 className="heading-6">My Toolkit</h2>
           <ul className={`${styles.toolkitList} unstyled-list`}>
             {
@@ -24,10 +37,16 @@ export default function HomeIntro() {
               })
             }
           </ul>
-        </div>
+        </motion.div>
       </div>
     </Container>
-    <VideoPlayer className={styles.introVideo} videoUrl="video/rainbow-bg-hero.mp4" />
+    <motion.div 
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.5, duration: 0.8 }}
+    >
+      <VideoPlayer className={styles.introVideo} videoUrl="video/rainbow-bg-hero.mp4" />
+    </motion.div>
   </div>
   );
 }
