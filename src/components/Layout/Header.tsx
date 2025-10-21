08b51463e984e9deb83f018924d15a926c6bc987
Emily Duttinger <emily.duttinger@zaneray.com>
2025-10-20 21:59:53 -0400

    Adjust header variables, theme toggle mobile positioning

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from "../UI/ThemeToggle";
 
 export default function Header() {
   return (
-    <header className="flex flex-col justify-center items-center text-center gap-[1.5rem] px-[var(--container-padding)] py-[1.5rem] sm:flex-row sm:justify-between sm:text-left sm:px-[2.5rem] relative z-100">
+    <header className="flex flex-col justify-center items-center text-center gap-[1.5rem] px-[var(--container-padding)] py-[1.5rem] md:flex-row md:justify-between md:text-left md:px-[2.5rem] relative z-100">
       <Link href="/" className="heading-3 m-0 font-black uppercase unstyled-link">Emily Duttinger</Link>
       <div className="flex items-center gap-[2.5rem]">
         <Navigation />
